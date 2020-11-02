25573270bf053df5cf6bb4ca572a125561baba7f
Rahul Choudhary <rahul0101rock@gmail.com>
2020-11-02 11:48:01 +0530

    Added Rainfall in Model

--- Crop Production Predictor.py
+++ Crop Production Predictor.py
@@ -1,4 +1,3 @@
-import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,12 +7,12 @@ from sklearn.ensemble import RandomForestRegressor
 plt.rc("font", size=14)
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
-df = pd.read_csv("apy.csv")
+df = pd.read_csv("Crop Production With Rainfall.csv")
+rain = pd.read_csv("Rainfall Predicted.csv")
 data = df.dropna()
-sum_maxp = data["Production"].sum()
-data["percent_of_production"] = data["Production"].map(lambda x:(x/sum_maxp)*100)
 print(data.shape)
 dis=input("Enter the District Name: ")
+state=list(data[data["District_Name"]==dis.upper()]["State_Name"][:1])[0]
 season=input("Enter the Season: ")
 area_in=input("Enter Area in hectares: ")
 s=list(data["Season"].unique())
@@ -28,14 +27,13 @@ y = data_dum[["Production"]]
 
 model = RandomForestRegressor()
 model.fit(x,y.values.ravel())
-print("Training Done...")
 
 ch=pd.DataFrame()
-ch_index=[]
 for crop in list(data_cu["Crop"].unique()):
     t=(x[x["Crop_{}".format(crop)]==1])[:1]
     ch=pd.concat([ch,t])
 ch["Area"]=area_in
+ch["Rainfall"]=list(rain[rain["State_Name"]==state]["Rainfall"])[0]
 predict=model.predict(ch)
 crname=data.loc[ch.index]["Crop"]
 crdata= {'Crop': list(crname), 
@@ -45,11 +43,10 @@ crpro=crpro.sort_values(by=['Production'], ascending=False)
 print(crpro)
 fig=plt.figure()
 ax = fig.add_axes([0,0,1,1])
-tstr='Predicted Prodction in        District: '+dis.title()+'      Season: '+season.title()
+tstr='Predicted Production in        District: '+dis.title()+'      Season: '+season.title()
 ax.set_title(tstr,fontsize=15)
 ax.set_ylabel('Production in Tones', fontsize=14)
 ax.set_xlabel('Crop', fontsize=13)
 ax.bar(list(crpro["Crop"])[:5], list(crpro["Production"])[:5])
 plt.savefig('plot.png', dpi=400, bbox_inches='tight')
 im = Image.open('plot.png').show()
-
